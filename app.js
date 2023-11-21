1105c0b6f7397af116a1b58c4e5a57850d7ae18c
a.mihnov <a.mihnov@anlax.org>
2023-11-21 23:11:33 +0300

    factory function is work

--- app.js
+++ app.js
@@ -6,51 +6,27 @@ const GameBord = {
     ],
 } 
 
-const namePlayerOne = document.querySelector('#playerOne');
-const namePlayerTwo = document.querySelector('#playerTwo');
-
-function usersCreate (name, shose) {
-    return {name, shose}
+function createUser(name) {
+    return{ name }
 }
-
-const buttonZero = document.querySelectorAll('.zero');
-buttonZero.forEach((button) => {
-    button.addEventListener('click', () => {
-        const shose1 = button.textContent;
-        usersCreate(name, shose1);
-        console.log(usersCreate(name, shose1))
-    })
-})
-
-const buttonX = document.querySelectorAll('.cross');
-buttonX.forEach((button) => {
-    button.addEventListener('click', () => {
-        // const shose1 = button.textContent;
-        // usersCreate(name, shose1);
-        // console.log(usersCreate(name, shose1))
-    })
-})
 
 const formOne = document.getElementById('formOne');
+const formTwo = document.getElementById('formTwo')
 
-function addValueToUser() {
-    
-}
+
 
 formOne.addEventListener('submit', (e) => {
     e.preventDefault();
-    // const nameOne = namePlayerOne.Value;
-    // let chose1 = 1;
-    usersCreate(nameOne, shose1)
-    console.log(usersCreate(nameOne, shose1))
+    const namePerson = document.getElementById('name1').value;
+    createUser(namePerson);
+    console.log('1', createUser(namePerson))
+    formOne.reset();
 })
 
-
-
-// usersCreate.getFromDom('.zero')
-
-
-// let person1 = users('Jhon', 'x');
-// let person2 = users('Jhon2', '0');
-// person1.info();
-// person2.info();
+formTwo.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const namePerson = document.getElementById('name2').value;
+    createUser(namePerson);
+    console.log('2',createUser(namePerson));
+    formTwo.reset()
+})
