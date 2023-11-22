4a46a507f07dcf6850693c93a2a56dde0fa93dcf
a.mihnov <a.mihnov@anlax.org>
2023-11-22 16:07:05 +0300

    improve factory function

--- app.js
+++ app.js
@@ -6,27 +6,30 @@ const GameBord = {
     ],
 } 
 
-function createUser(name) {
-    return{ name }
+function createUser(id, html) {
+    const namePerson = document.getElementById(id);
+    const nameValue = namePerson.value;
+    const htmlName = document.getElementById(html);
+    htmlName.innerHTML = nameValue;
+    return{ 
+        nameValue,
+        htmlName,
+    }
 }
 
 const formOne = document.getElementById('formOne');
-const formTwo = document.getElementById('formTwo')
-
-
+const formTwo = document.getElementById('formTwo');
 
 formOne.addEventListener('submit', (e) => {
     e.preventDefault();
-    const namePerson = document.getElementById('name1').value;
-    createUser(namePerson);
-    console.log('1', createUser(namePerson))
+    createUser('name1', 'per1');
+    console.log('1', createUser('name1', 'per1'))
     formOne.reset();
 })
 
 formTwo.addEventListener('submit', (e) => {
     e.preventDefault();
-    const namePerson = document.getElementById('name2').value;
-    createUser(namePerson);
-    console.log('2',createUser(namePerson));
+    createUser('name2', 'per2');
+    console.log('2',createUser('name2', 'per2'));
     formTwo.reset()
 })
