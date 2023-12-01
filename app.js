e9bd96bcd78a1dde59d938701b193a10d72f0f5b
a.mihnov <a.mihnov@anlax.org>
2023-12-01 16:17:15 +0300

    try to create function createUser

--- app.js
+++ app.js
@@ -1,35 +1,46 @@
-const GameBord = {
+gamePlace = {
     game: [
         '', '', '',
         '', '', '',
         '', '', ''
-    ],
-} 
-
-function createUser(id, html) {
-    const namePerson = document.getElementById(id);
-    const nameValue = namePerson.value;
-    const htmlName = document.getElementById(html);
-    htmlName.innerHTML = nameValue;
-    return{ 
-        nameValue,
-        htmlName,
-    }
+    ]
 }
 
-const formOne = document.getElementById('formOne');
-const formTwo = document.getElementById('formTwo');
+// const userOne = document.getElementById('userOne');
+// let userNameOne = document.getElementById('userNameOne');
 
-formOne.addEventListener('submit', (e) => {
+
+function createUsers (name) {
+    return {
+        name,
+    };
+}
+
+const submit = document.getElementById('formOne');
+
+function addUserNameToBoard(){
+    // const userOne = document.getElementById('userOne');
+    const userOne = document.getElementById('userOne').value;
+    const userTwo = document.getElementById('userTwo').value
+
+    let userNameOne = document.getElementById('userNameOne');
+    // userNameOne.textContent = nameOne;
+    let userNameTwo = document.getElementById('userNmaetWO')
+    // userNameTwo = nameTwo;
+    const add = createUsers(`${userOne}`)
+    const add2 = createUsers(`${userTwo}`)
+    userNameOne = add;
+    userNameTwo = add2;
+}
+
+
+submit.addEventListener('click', (e) => {
     e.preventDefault();
-    createUser('name1', 'per1');
-    console.log('1', createUser('name1', 'per1'))
-    formOne.reset();
+    addUserNameToBoard()
+    // createUsers(e)
+    console.log('user:', createUsers(e));
+    console.log('name:', addUserNameToBoard(e));
 })
 
-formTwo.addEventListener('submit', (e) => {
-    e.preventDefault();
-    createUser('name2', 'per2');
-    console.log('2',createUser('name2', 'per2'));
-    formTwo.reset()
-})
+// const add = createUsers("Anton")
+// console.log(add)
