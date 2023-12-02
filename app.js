9c1d5c6f1f8c4b40c04ef1157616f84a7dbed2c2
a.mihnov <a.mihnov@anlax.org>
2023-12-02 17:45:08 +0300

    can add '1' in array but only in start

--- app.js
+++ app.js
@@ -38,7 +38,51 @@ const form = document.getElementById('form');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     addNameInObj();
-    addNameToPage()
+    addNameToPage();
     console.log(userOneInfo)
     console.log(userTwoInfo)
+})
+
+let Counter = (function () {
+    let count = 0;
+
+    function changBy(val) {
+        count += val;
+    }
+
+    return {
+        add: function() {
+            changBy(1);
+        },
+
+        value: function() {
+           return count;
+        },
+    };
+})();
+
+function strokeChange() {
+    Counter.add();
+    return Counter.value();
+} 
+
+const item = document.querySelectorAll('.item');
+
+function fillAnObject() {
+    let arr = gamePlace.game;
+    for (let i = 0; i < arr.length -1; i++) {
+        // if (arr[i] == item.id){
+        //     arr.splice(1, 1, '1');
+        // }
+        arr.splice(arr[i], 1, '1')
+    }
+    // for( let i = 0; i<)
+}
+
+item.forEach((item) => {
+    item.addEventListener('click', () => {
+        console.log('value:',strokeChange());
+        fillAnObject();
+        console.log(gamePlace);
+    })
 })
