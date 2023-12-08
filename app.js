2731c14fc0bdfb97d6b8e9b0f1aa00e6f81ff6fe
a.mihnov <a.mihnov@anlax.org>
2023-12-08 19:14:26 +0300

    Try four

--- app.js
+++ app.js
@@ -6,19 +6,13 @@ const gameBoard = {
     ]
 }
 
-function setPlayerName () {
-    const playerNameOne = document.getElementById('userOne').value;
-    const playerNameTwo = document.getElementById('userTwo').value;
-    return {
-        playerNameOne,
-        playerNameTwo,
-    }
-}
 
-function createPlayers () {
+
+function gameControl () {
+    const item = document.querySelectorAll('item');
     const playerNameOne = document.getElementById('userOne');
     const playerNameTwo = document.getElementById('userTwo');
-    return players = [
+    const players = [
         {
             name: playerNameOne.value,
             token: '1',
@@ -29,38 +23,70 @@ function createPlayers () {
         }
     ]
 
-}
+    let activePlayer = players[0];
 
-let activePlayer = players[0];
-
-function indentifyTurn () {
-    if (activePlayer === players[0]) {
-        return players[1];
-    } else {
-        return players[0];
-    }
-}
-
-function pushInObject (item) {
-    const place = gameBoard.game;
-    for (let i = 0; i < place.length; i++) {
-        if (place[i] === item.id) {
-            place.splice(1, 1, activePlayer.token);
+    function indentifyTurn () {
+        if (activePlayer === players[0]) {
+            return players[1];
+        } else {
+            return players[0];
         }
     }
+
+    const userNameOne = document.getElementById('userNameOne');
+    const userNameTwo = document.getElementById('userNameTwo');
+
+    userNameOne.textContent = players[0].name;
+    userNameTwo.textContent = players[1].name;
+
+    const getActivePlayer = () => activePlayer;
+    
+    const addMarker = () => {
+        if(getActivePlayer === players[0]){
+            userNameTwo.classList.remove('activePlayer');
+            userNameOne.classList.add('activePlayer');
+        } else {
+            userNameOne.classList.remove('activePlayer');
+            userNameTwo.classList.add('activePlayer');
+        }
+    }
+
+    
+    function pushInObject (item) {
+        const place = gameBoard.game;
+        for (let i = 0; i < place.length; i++) {
+            if (place[i] === item.id) {
+                place.splice(1, 1, activePlayer.token);
+            }
+        }
+    }
+
+    item.forEach((item) => {{
+        item.addEventListener('click', () => {
+            gameControl();
+            pushInObject();
+        })
+    }})
+
+    return {
+        indentifyTurn,
+        pushInObject,
+        addMarker,
+    }
+
 }
 
-const item = document.querySelectorAll('item');
-item.forEach((item) => {{
-    item.addEventListener('click', () => {
-        indentifyTurn();
-        pushInObject();
-    })
-}})
+// const item = document.querySelectorAll('item');
+// item.forEach((item) => {{
+//     item.addEventListener('click', () => {
+//         gameControl();
+//         pushInObject();
+//     })
+// }})
 
 const form = document.getElementById('form');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(createPlayers());
+    console.log(gameControl());
 })
